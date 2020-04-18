c3c6953650bf5ad59b9356a06dc9635d3e7e9633
morenon1118 <tervzmoreno@gmail.com>
2020-04-18 12:50:29 -0500

    commit 4.18.20

--- code.R
+++ code.R
@@ -1,3 +1,7 @@
+library(rvest)
+library(dplyr)
+
+
 #upsert
 dat.std <- read.csv("/Users/Computer/Downloads/MLS data standard.csv")
 dat.gls <- read.csv("/Users/Computer/Downloads/MLS data goals.csv")
@@ -15,5 +19,24 @@ dat.raw <- merge(dat.raw, dat.pos, by = c("Player"))
 #dedupe
 MLS <- dat.raw[!duplicated(dat.raw$Player), ]
 
+#clean and refine to find players who scored a goal
+MLS$Player <- as.character(MLS$Player)
+MLS <- MLS[MLS$Gls. > 0, ]
+
+
 #export
-write.csv(MLS, "/Users/Computer/Documents/2019MLS.csv", row.names = FALSE)
+# write.csv(MLS, "/Users/Computer/Documents/2019MLS.csv", row.names = FALSE)
+# write.csv(MLS, "/Users/Computer/Documents/2019MLS.csv", row.names = FALSE)
+
+url <- "https://www.thebluetestament.com/2019/6/13/18678329/complete-2019-mls-major-league-soccer-salaries-millionaires-sporting-kc"
+
+#Bringing in player salary
+salary <- read.table("/Users/Computer/Desktop/MLS19 salary.txt", sep = "\t")
+# salary$Player <- paste(salary$V1, salary$V2)
+# salary$V1 <- salary$Player
+# salary <- salary[, -c(5)]
+colnames(salary) <- c("Player", "Team", "Pos", "Salary")
+
+MLS.new <- left_join(MLS, salary, by = "Player")
+
+
